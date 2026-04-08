bc5f47703332ec94d16549785a810d3f87827d7e
anisyavieen <anisyamiftahuljannah17@gmail.com>
2026-04-08 08:25:47 +0700

    admin update

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -149,20 +149,24 @@ class="hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w
 
             <!-- Body -->
             <form class="mt-4 space-y-3">
-                <input type="text" placeholder="Nama Event"
+                <input type="text" placeholder="Judul Event"
                     class="w-full border p-2 rounded">
-
-                <input type="number" placeholder="Harga"
-                    class="w-full border p-2 rounded">
-
-                <select class="w-full border p-2 rounded">
-                    <option>Pilih Kategori</option>
-                    <option>Seminar</option>
-                    <option>Workshop</option>
-                </select>
-
                 <textarea placeholder="Deskripsi"
                     class="w-full border p-2 rounded"></textarea>
+                <input type="date" placeholder="Tanggal"
+                    class="w-full border p-2 rounded">
+                <input type="time" placeholder="Waktu"
+                    class="w-full border p-2 rounded">
+                <input type="text" placeholder="Lokasi"
+                    class="w-full border p-2 rounded">
+                <select class="w-full border p-2 rounded">
+                    <option>Pilih Kategori</option>
+                    <option>Akademik</option>
+                    <option>Non-Akademik</option>
+                </select>
+                <input type="file" placeholder="Foto"
+                    class="w-full border p-2 rounded">
+                </form>
 
                 <!-- Footer -->
                 <div class="flex justify-end gap-2 pt-3">
@@ -181,69 +185,7 @@ class="hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w
 
         </div>
     </div>
-</div>div>
-
-<!-- Modal toggle -->
-<button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" type="button">
-  Toggle modal
-</button>
-
-<!-- Main modal -->
-<div id="crud-modal" tabindex="-1" aria-hidden="true"
-class="hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-white/80 backdrop-blur-sm"></div>
-    <div class="relative p-4 w-full max-w-md max-h-full">
-
-        <!-- Modal content -->
-        <div class="relative bg-white rounded shadow-lg p-4 md:p-6">
-
-            <!-- Modal header -->
-            <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
-                <h3 class="text-lg font-medium text-heading">
-                    Create new product
-                </h3>
-                <button type="button" class="text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center" data-modal-hide="crud-modal">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-
-            <!-- Modal body -->
-            <form action="#">
-                <div class="grid gap-4 grid-cols-2 py-4 md:py-6">
-                    <div class="col-span-2">
-                        <label for="name" class="block mb-2.5 text-sm font-medium text-heading">Name</label>
-                        <input type="text" name="name" id="name" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="Type product name" required="">
-                    </div>
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="price" class="block mb-2.5 text-sm font-medium text-heading">Price</label>
-                        <input type="number" name="price" id="price" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="$2999" required="">
-                    </div>
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="category" class="block mb-2.5 text-sm font-medium text-heading">Category</label>
-                        <select id="category" class="block w-full px-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-3 py-2.5 shadow-xs placeholder:text-body">
-                            <option selected="">Select category</option>
-                            <option value="TV">TV/Monitors</option>
-                            <option value="PC">PC</option>
-                            <option value="GA">Gaming/Console</option>
-                            <option value="PH">Phones</option>
-                        </select>
-                    </div>
-                    <div class="col-span-2">
-                        <label for="description" class="block mb-2.5 text-sm font-medium text-heading">Product Description</label>
-                        <textarea id="description" rows="4" class="block bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full p-3.5 shadow-xs placeholder:text-body" placeholder="Write product description here"></textarea>                    
-                    </div>
-                </div>
-                <div class="flex items-center space-x-4 border-t border-default pt-4 md:pt-6">
-                    <button type="submit" class="inline-flex items-center  text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-                        <svg class="w-4 h-4 me-1.5 -ms-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/></svg>
-                        Add new product
-                    </button>
-                    <button data-modal-hide="crud-modal" type="button" class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Cancel</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div> 
+</div>
 </div>
 
 </body>
