2c294ba199ba06dc3007fb9db7c5574e992b2c6b
Jastin-vergil <jastinreja@gmail.com>
2026-07-10 22:21:45 +0700

    rapihkan routes

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@ use App\Http\Controllers\PembayaranController;
 use App\Http\Controllers\TiketController;
 use Illuminate\Support\Facades\Route;
 
+// Auth
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('admin.login.submit');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+// Dashboard
 Route::get('/userdashboard', [DashboardController::class, 'index'])
     ->name('user.dashboard');
 
@@ -20,6 +22,7 @@ Route::get('/', function () {
     return redirect()->route('user.dashboard');
 });
 
+// CRUD Tiket (admin)
 Route::resource('tiket', TiketController::class)
     ->except(['show'])
     ->names([
@@ -31,6 +34,7 @@ Route::resource('tiket', TiketController::class)
         'destroy' => 'admin.ticket.destroy',
     ]);
 
+// CRUD Event (admin)
 Route::resource('events', EventController::class)
     ->except(['show'])
     ->names([
@@ -42,6 +46,7 @@ Route::resource('events', EventController::class)
         'destroy' => 'admin.event.destroy',
     ]);
 
+// CRUD Kategori (admin, pakai modal jadi create/edit gak ada)
 Route::resource('kategori', KategoriController::class)
     ->except(['show', 'create', 'edit'])
     ->names([
@@ -51,26 +56,33 @@ Route::resource('kategori', KategoriController::class)
         'destroy' => 'admin.kategori.destroy',
     ]);
 
+// Riwayat transaksi
 Route::get('/transactionhistory', function () {
     return view('Pages.transaction_history');
 })->name('transaction.history');
 Route::get('/transaction/search', [PembayaranController::class, 'searchByEmail'])->name('transaction.search');
 
+// Pembayaran
 Route::get('/pembayaran', [PembayaranController::class, 'index'])->name('admin.pembayaran.index');
 Route::get('/pembayaran/{pembayaran}', [PembayaranController::class, 'show'])->name('admin.pembayaran.show');
 Route::post('/pembayaran', [PembayaranController::class, 'store'])->name('payment.store');
 Route::post('/pembayaran/{pembayaran}/approve', [PembayaranController::class, 'approve'])->name('admin.pembayaran.approve');
 Route::post('/pembayaran/{pembayaran}/reject', [PembayaranController::class, 'reject'])->name('admin.pembayaran.reject');
+Route::delete('/pembayaran/{pembayaran}', [PembayaranController::class, 'destroy'])->name('admin.pembayaran.destroy');
 
+// Form beli tiket
 Route::get('/ticket-form', [TiketController::class, 'ticketForm'])->name('ticket.form');
 
+// Halaman payment
 Route::get('/payment', function () {
     return view('Pages.payment');
 })->name('payment');
 
+//route 'payment.store'
 Route::post('/payment.store', [PembayaranController::class, 'store'])
     ->name('payment.store');
 
+// Peserta event
 Route::get('/admin/participants', [AdminParticipantController::class, 'index'])
     ->name('admin.participants');
 
